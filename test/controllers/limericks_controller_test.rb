006fe668c4e10696a4ae68e424f12da53a18a370
Scott Fleming <sdflem@gmail.com>
2023-11-25 15:43:35 -0600

    Reset controller tests to original form

--- test/controllers/limericks_controller_test.rb
+++ test/controllers/limericks_controller_test.rb
@@ -1,13 +1,7 @@
 require "test_helper"
 
 class LimericksControllerTest < ActionDispatch::IntegrationTest
-
-  test "should get index" do
-    # Arrange (nothing to do for this test)
-    # Act
-    get root_url
-    # Assert
-    assert_response :success
-  end
-
+  # test "the truth" do
+  #   assert true
+  # end
 end
--- test/controllers/user_limericks_controller_test.rb
+++ test/controllers/user_limericks_controller_test.rb
@@ -1,28 +1,7 @@
 require "test_helper"
 
 class UserLimericksControllerTest < ActionDispatch::IntegrationTest
-
-  include Devise::Test::IntegrationHelpers
-
-  test "should get index for signed-in user" do
-    # Arrange
-    user = users(:one)
-    sign_in user
-    # Act
-    get user_limericks_url(user)
-    # Assert
-    assert_response :success
-  end
-
-  test "should get index for user other than signed-in user" do
-    # Arrange
-    user_one = users(:one)
-    sign_in user_one
-    user_two = users(:two)
-    # Act
-    get user_limericks_url(user_two)
-    # Assert
-    assert_response :success
-  end
-
+  # test "the truth" do
+  #   assert true
+  # end
 end
