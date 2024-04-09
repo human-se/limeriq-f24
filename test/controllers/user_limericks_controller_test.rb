5f6fed335924e7ad77f45b9ab20f5a179d7caabd
Scott Fleming <sdflem@gmail.com>
2024-04-09 10:59:49 -0500

    Add a controller test for

--- test/controllers/user_limericks_controller_test.rb
+++ test/controllers/user_limericks_controller_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class UserLimericksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  test "should get index" do
+    user = users(:one)
+    limericks_size = user.limericks.size
+    sign_in user
+    get user_limericks_url(user)
+    assert_response :success
+    assert_select 'div.card', limericks_size
+  end
+
 end
