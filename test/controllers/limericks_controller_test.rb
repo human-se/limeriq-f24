d4a1214726ccbeb35022dd32699e616a8f459820
Scott Fleming <sdflem@gmail.com>
2024-04-09 10:42:15 -0500

    Add a controller test for

--- test/controllers/limericks_controller_test.rb
+++ test/controllers/limericks_controller_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class LimericksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should get index" do
+    limericks_size = limericks.size
+    get root_url
+    assert_response :success
+    assert_select 'div.card', limericks_size
+  end
+
 end
